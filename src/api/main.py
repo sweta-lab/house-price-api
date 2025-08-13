a55332e03009a3e77369e2745458cbb2c219b464
Sweta Rajan <swetar128@gmail.com>
2025-08-13 09:04:25 +0200

    remove hardcoded model path to ensure deployment works

--- src/api/main.py
+++ src/api/main.py
@@ -8,7 +8,7 @@ from src.monitoring.logger import setup_logger
 app = Flask(__name__)
 logger = setup_logger()
 
-model_path = 'C:\\Users\\sweta\\Documents\\Python_Scripts\\house-price-api\\house-price-api\\models\\house_price_model.pkl'
+model_path = 'models\\house_price_model.pkl'
 model = load_model(model_path)
 
 @app.route('/predict', methods=['POST'])
